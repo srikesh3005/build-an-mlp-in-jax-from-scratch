4b3bfc30fafa27e0085b8ae6bd9865b8db5aa15f
Srikesh <81523080+srikesh3005@users.noreply.github.com>
2026-08-23 20:51:55 +0530

    Step 4: sample_input_features

--- model.py
+++ model.py
@@ -31,8 +31,12 @@ import jax.numpy as jnp
 def sample_normal_matrix(key, shape):
     return jax.random.normal(key,shape)
 
-# Step 4 - sample_input_features (not yet solved)
-# TODO: implement
+# Step 4 - sample_input_features
+import jax
+import jax.numpy as jnp
+
+def sample_input_features(key, batch_size, num_features):
+    return sample_normal_matrix(key,(batch_size,num_features))
 
 # Step 5 - assign_class_labels (not yet solved)
 # TODO: implement
