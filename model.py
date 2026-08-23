ea0dbc5c6631941578b6de2a19f52b1da6192446
Srikesh <81523080+srikesh3005@users.noreply.github.com>
2026-08-23 20:44:36 +0530

    Step 3: sample_normal_matrix

--- model.py
+++ model.py
@@ -24,8 +24,12 @@ import jax
 def split_prng_key(key, num):
     return jax.random.split(key,num)
 
-# Step 3 - sample_normal_matrix (not yet solved)
-# TODO: implement
+# Step 3 - sample_normal_matrix
+import jax
+import jax.numpy as jnp
+
+def sample_normal_matrix(key, shape):
+    return jax.random.normal(key,shape)
 
 # Step 4 - sample_input_features (not yet solved)
 # TODO: implement
